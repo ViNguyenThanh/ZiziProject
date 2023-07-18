4c9e850950c1d7b12faa3d67d74f1f537e799993
ViNguyenThanh <vint011103@gmail.com>
2023-07-18 20:22:56 +0700

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello world");
+
+//git add tên file: để lưu file vào git
+//git add . : để lưu cả thư mục vào git
+//git commit -m "message" : lưu message
+//git status: kiểm tra trạng thái git
+//git log: Hiện các thay đổi của project
